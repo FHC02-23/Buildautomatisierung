ff9093b9ad3484e625e0f55fe183438a7ada98a4
Daniel Zettel <daniel.zettel@lv.campus02.at>
2024-02-02 16:20:39 +0100

    added test template and dependency

---
+++ src/test/java/org/campus02/zettel/NotenverteilungTest.java
@@ -0,0 +1,14 @@
+package org.campus02.zettel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NotenverteilungTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addStudent() {
+    }
+}
